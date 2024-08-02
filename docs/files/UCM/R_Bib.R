e83d371c99ffce55a9f8c5e0bb0f172070ab8c1b
Isaac QS <inqs.909@gmail.com>
2024-08-01 23:08:12 -0700

    Updated Files

--- docs/files/UCM/R_Bib.R
+++
@@ -1,9 +0,0 @@
-library(knitr)
-library(tidyverse)
-library(xaringan)
-library(xaringanthemer)
-library(parallel)
-library(lme4)
-library(mvtnorm)
-
-write_bib(file="RBIB.bib")
--- docs/files/niss_1/niss_1.R
+++
@@ -1,6 +0,0 @@
-library(xaringanthemer)
-style_mono_accent(
-  base_color = "#0b5345",
-  base_font_size = "22px",
-  outfile = "files/niss_1/styles.css"
-)
