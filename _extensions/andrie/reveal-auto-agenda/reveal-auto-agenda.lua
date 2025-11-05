0a557029bb7382b4e681dc07fd68d8a05f812407
Isaac QS <inqs.909@gmail.com>
2025-11-05 08:47:56 -0800

    Updates

--- _extensions/andrie/reveal-auto-agenda/reveal-auto-agenda.lua
+++ _extensions/andrie/reveal-auto-agenda/reveal-auto-agenda.lua
@@ -1,19 +1,28 @@
 
-local dump = quarto.utils.dump
 local stringify = pandoc.utils.stringify
 local headers = pandoc.List()
+local header_ids = pandoc.List()
 
 local options_bullets = "bullet"
 local options_heading = nil
+local options_clickable = false
+
+function is_truthy(val)
+  if val == nil then
+    return false
+  else
+    local str_val = stringify(val):lower()
+    return str_val == "true" or str_val == "1" or str_val == "yes" or str_val == "on"
+  end
+end
 
 -- permitted options include:
 -- auto-agenda:
 --   bullets: none | bullet | numbered
 --   heading: none | heading
+--   clickable: true | false
 local function read_meta(meta)
   local options = meta["auto-agenda"]
-  -- options_bullets = "none"
-  -- if options == nil then return meta end
   if options ~= nil then
     if options.bullets ~= nil then
       options_bullets = stringify(options.bullets)
@@ -23,21 +32,30 @@ local function read_meta(meta)
     if options.heading ~= nil then
       options_heading = options.heading
     end
+    options_clickable = is_truthy(options.clickable)
   end
+  -- print("Configured options: bullets=" .. stringify(options_bullets) .. ", heading=" .. stringify(options_heading) .. ", clickable=" .. stringify(options_clickable))
 end
 
 local function get_header_text(el)
   return el.content
 end
 
+local function get_header_id(el)
+  return el.identifier or ""
+end
+
 local function scan_headers(el)
-  if el.level == 1 then
+  if el.level == 1  then
     headers:insert(get_header_text(el))
+    header_ids:insert(get_header_id(el))
   end
 end
 
+--@param el pandoc.Header
+--@return pandoc.Header
 local function change_header_class(el)
-  el.attr = pandoc.Attr("", {"agenda-slide"})
+  el.attr.classes = {"agenda-slide"}
   return el
 end
 
@@ -53,7 +71,6 @@ local function scan_blocks(blocks)
   local bullet_class = pandoc.BulletList
   if options_bullets ~= nil then
     if options_bullets == "none" then
-    -- print(options_bullets)
     bullet_class = identity
     elseif options_bullets == "numbered" then
       bullet_class = pandoc.OrderedList
@@ -61,7 +78,11 @@ local function scan_blocks(blocks)
   end
 
   for _, block in pairs(blocks) do
-    if (block ~= nil and block.t == "Header" and block.level == 1) then
+    if (block ~= nil and 
+      block.t == "Header" and 
+      block.level == 1 and 
+      not block.attr.classes:includes("no-auto-agenda")
+    ) then
       header_n = header_n + 1
       change_header_class(block)
       newBlocks:insert(block)
@@ -87,8 +108,18 @@ local function scan_blocks(blocks)
         elseif (i > header_n) then
           agenda_class = {"agenda-inactive", "agenda-post-active"}
         end
+
+        local list_item_el = nil
+        if options_clickable then
+          table.insert(agenda_class, "agenda-clickable")
+          link_href = "#" .. header_ids[i]
+          list_item_el = pandoc.Link(headers[i], link_href)
+        else
+          list_item_el = pandoc.Para(headers[i])
+        end
+
         mod_headers:insert(
-          pandoc.Div(pandoc.Para(headers[i]), pandoc.Attr("", agenda_class))
+          pandoc.Div(list_item_el, pandoc.Attr("", agenda_class))
         )
       end
           
@@ -107,7 +138,7 @@ local function scan_blocks(blocks)
   -- inject the CSS dependency
   quarto.doc.addHtmlDependency({
     name = "reveal-auto-agenda",
-    version = "0.0.1",
+    version = "0.0.3",
     stylesheets = {"reveal-auto-agenda.css"}
   })
 
